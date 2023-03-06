54f54e6f62263acb3dc7c6c60a3b88c705c89a65
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-06 16:20:09 -0500

    ejercicio2_calcular_el_area_de_un_circulo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
 fun main(){
-    
+    var radio: Int = 0
+    var PI : Double = 3.14
+
+    tituloPrincipal()
+    tituloRadio()
+    radio = readLine()!!.toInt()
+    println("El area del circulo es -> ${calcularArea(radio = radio, PI = PI)}")
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,10 @@
+const val PI: Double = 3.14
+fun tituloPrincipal(){
+    println("Calcular el area de un circulo")
+}
+fun tituloRadio(){
+    println("Dame el radio")
+}
+fun calcularArea(PI: Double, radio:Int): Double {
+return (radio*radio*PI)
+}
