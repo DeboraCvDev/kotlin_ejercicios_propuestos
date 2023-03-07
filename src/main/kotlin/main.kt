83291803f5be68356fa5130870cb607d17906baa
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-07 14:48:28 -0500

    ejercicio7_Calcular_el_salario_quincenal

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
-fun main(){
-    
+fun main() {
+    var primerNumero: Int = 0
+    var segundoNumero: Double = 0.1
+
+    tituloPrincipal()
+    horasTrabajadas()
+    primerNumero = readLine()!!.toInt()
+    salarioPorHora()
+    segundoNumero = readLine()!!.toDouble()
+    println("El salario quincelas por las horas trbajadas es ${calcularElSalarioQuincenal(primerNumero = primerNumero,segundoNumero=segundoNumero)}")
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,12 @@
+fun tituloPrincipal(){
+    println("Calcular el salario quincenal")
+}
+fun horasTrabajadas(){
+    println("Ingresar las horas trabajadas:")
+}
+fun salarioPorHora(){
+    println("Ingresar El salario por hora:")
+}
+fun calcularElSalarioQuincenal(primerNumero: Int,segundoNumero:Double):Double{
+    return primerNumero*segundoNumero
+}
