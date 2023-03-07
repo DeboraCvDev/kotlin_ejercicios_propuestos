4ae5d72678686e44c4498e75d8bb5becdd4cb069
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-07 14:10:54 -0500

    ejercicio5_Hallar_la_cantidad_de_números_que_se_encuentran_en_un_intervalo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main(){
-    
+    var numeroMayor: Int = 0
+    var numeroMenor: Int = 0
+
+    tituloPrincipal()
+    ingresarElNumero()
+    numeroMayor = readLine()!!.toInt()
+    numeroMenor = readLine()!!.toInt()
+    println("Los numeros incluidos dentro de $numeroMayor y $numeroMenor son un total de ${hallarElIntervalo(numeroMayor = numeroMayor, numeroMenor = numeroMenor)}")
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,9 @@
+fun tituloPrincipal(){
+    println("Dado dos numero enteros, determinar cuantos numeros enteros estan incluidos entre ellos")
+}
+fun ingresarElNumero(){
+    println("Ingrese los numero:")
+}
+fun hallarElIntervalo(numeroMayor: Int,numeroMenor: Int):Int{
+    return (numeroMenor-numeroMayor)-1
+}
