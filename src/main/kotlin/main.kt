051322281b18d264cca96ba074b9e94f552e6220
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-07 16:02:39 -0500

    ejercicio9_Hallar_el_término_de_la_sucesión_de_segundo_grado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,36 @@
 fun main(){
-    
+    var primerNumero:  Int = 0
+    var segundoNumero:  Int = 0
+    var tercerNumero:  Int = 0
+    var cuartoNumero:  Int = 0
+    var numeroTermino: Int = 0
+    var razon1:Int =0
+    var razon2: Int =0
+    var t0: Int =0
+    var a: Int = 0
+    var b: Int = 0
+    var c: Int = 0
+    var rpta: Int = 0
+
+
+    tituloPrincipal()
+    ingresarPrimerNumero()
+    primerNumero = readLine()!!.toInt()
+    ingresarSegundoNumero()
+    segundoNumero = readLine()!!.toInt()
+    ingresarTercerNumero()
+    tercerNumero = readLine()!!.toInt()
+    ingresarCuartoNumero()
+    cuartoNumero = readLine()!!.toInt()
+    terminoAHallar()
+    numeroTermino = readLine()!!.toInt()
+
+     razon1 = (cuartoNumero-tercerNumero) - (tercerNumero-segundoNumero)
+     razon2 = (segundoNumero - primerNumero) -razon1
+     t0 = primerNumero - razon2
+     a = razon1/2
+     b = razon2-a
+     c = t0
+    rpta = a * (numeroTermino * numeroTermino) + (b * numeroTermino) + c
+    println("El termino $numeroTermino es igual a $rpta")
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,18 @@
+fun tituloPrincipal(){
+    println("Hallar el término de la sucesión de segundo grado")
+}
+fun ingresarPrimerNumero(){
+    println("Ingresar el primer numero:")
+}
+fun ingresarSegundoNumero(){
+    println("Ingresar el segundo numero:")
+}
+fun ingresarTercerNumero(){
+    println("Ingresar el tercer numero:")
+}
+fun ingresarCuartoNumero(){
+    println("Ingresar el cuarto numero:")
+}
+fun terminoAHallar(){
+    println("¿Que terrmino desea hallar?:")
+}
