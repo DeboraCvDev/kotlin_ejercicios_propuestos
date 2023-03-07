9075058bd4896de7200dad506e07a0f77d71bf08
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-07 16:20:28 -0500

    ejercicio10_Calcular_las_raíces_de_una_ecuación_de_segundo_grado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,25 @@
 fun main(){
-    
+    var a : Int = 0
+    var b : Int = 0
+    var c : Int =0
+    var raiz : Int = 0
+    var b1 : Int = 0
+    var a1 : Int = 0
+
+    tituloPrincipal()
+    coeficienteDeSegundoGrado()
+    a = readLine()!!.toInt()
+    coeficienteDePrimerGrado()
+    b = readLine()!!.toInt()
+    coeficienteDeGradoCero()
+    c = readLine()!!.toInt()
+
+    raiz = (b * b) - 4 * (a * c)
+    b1 = b * (-1)
+    a1 = 2 * a
+
+    println("Las raíces son: ")
+    println("La raiz 1 es: $b1 + √$raiz / $a1")
+    println("La raiz 2 es: $b1 - √$raiz / $a1")
+
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,12 @@
+fun tituloPrincipal(){
+    println("Calcular las raíces de una ecuación de segundo grado")
+}
+fun coeficienteDeSegundoGrado(){
+    println("Ingresar coeficiente de segundo grado:")
+}
+fun coeficienteDePrimerGrado(){
+    println("Ingresar coeficiente de primer grado:")
+}
+fun coeficienteDeGradoCero(){
+    println("Ingresar coeficiente de grado cero:")
+}
