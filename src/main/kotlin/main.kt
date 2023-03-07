c0000b378112e1b0b3c1572e6a3677ccb6b22ec6
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-07 13:56:39 -0500

    ejercicio4_Convertidor_de_horas_a_minutos_y_segundos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
 fun main(){
-    
+    var primerNumero: Int = 0
+
+    tituloPrincipal()
+    ingresarHoras()
+    primerNumero = readLine()!!.toInt()
+    println("$primerNumero horas es igual a ${convertidorAMinutos(primerNumero = primerNumero)} minutos")
+    println("$primerNumero horas es igual a ${convertidorAsegundos(primerNumero = primerNumero)} segundos")
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,15 @@
+fun tituloPrincipal() {
+    println("Convertidor de horas a minutos y segundos")
+}
+
+fun ingresarHoras() {
+    println("Ingresa el numero:")
+}
+
+fun convertidorAMinutos(primerNumero: Int): Int {
+    return primerNumero * 60
+}
+
+fun convertidorAsegundos(primerNumero: Int): Int {
+    return primerNumero * 3600
+}
