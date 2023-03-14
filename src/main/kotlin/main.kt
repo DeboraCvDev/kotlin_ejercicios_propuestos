19effa435e686545f8bc97a864f6161374ba478b
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-14 16:26:16 -0500

    ejercicio27_Calcular_cantidad_de_pares_e_impares_sin_contar_a_múltiplos_de_5

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main(){
-    
+    var numero: Int
+    var numerosPares = 0
+    var numerosImpares = 0
+
+    tituloPrincipal()
+    ingresarTotalDeNumero()
+    numero = readLine()!!.toInt()
+    for (i in 1..numero){
+        println("Ingrese el número $i: ")
+        numero = readLine()!!.toInt()
+        if (numero % 5 != 0){
+            if (numero % 2 == 0)numerosPares++ else numerosImpares++
+        }
+    }
+    println("En total hay $numerosPares números pares y $numerosImpares números impares")
 }
---
+++ src/main/kotlin/utilitarios.kt
@@ -0,0 +1,6 @@
+fun tituloPrincipal(){
+    println("Calcular cantidad de pares e impares sin contar a múltiplos de 5")
+}
+fun ingresarTotalDeNumero(){
+    println("Ingrese el total de numeros que desea ingresar: ")
+}
