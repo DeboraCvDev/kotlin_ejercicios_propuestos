1253d57475e211b53eb47665d1f8ea13427974d9
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-14 16:36:40 -0500

    ejercicio28_Calcular_el_factorial_de_un_número

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main(){
-    
+    println("Calcular el factorial de un número")
+    println("Ingrese el número: ")
+    var numero = readLine()!!.toInt()
+
+    for (i in numero-1 downTo 1){
+        numero *= i
+    }
+    println("El factorial es: $numero")
 }
