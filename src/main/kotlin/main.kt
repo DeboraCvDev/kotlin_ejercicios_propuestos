6857722ba2d9cf8869ffd9802a76e4b190c68232
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-14 16:16:52 -0500

    ejercicio26_Ingresar_10_números_y_calcular_la_suma

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(){
-    
+    var primerNumero: Int
+    var suma: Int = 0
+
+    for (i in 1..10){
+        println("Ingrese el número $i")
+        primerNumero = readLine()!!.toInt()
+        suma+= primerNumero
+    }
+    println("La suma es $suma")
 }
