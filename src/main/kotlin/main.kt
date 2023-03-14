11359ebfdc8e8f3d5841fe70884a6028620513cc
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-14 16:42:15 -0500

    ejercicio29_Calcular_el_total_de_números_ingresados_por_el_usuario

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    
+    println("Calcular el total de números ingresados por el usuario:")
+    var numero = 5
+    var contador = 0
+
+    while (numero != 0){
+        println("Ingrese un número: ")
+        numero = readLine()!!.toInt()
+        if (numero != 0) contador++
+    }
+    println("Resultado esperado es: $contador")
 }
